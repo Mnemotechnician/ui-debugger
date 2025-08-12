0a95d4b011f3cc596161e6ea4e52793cbe110ef4
nekit508 <lastp508@gmail.com>
2025-08-12 11:17:57 +0300

    (-_-(

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-println("Now using ${JavaVersion.current().name} java version for building.")
+println("Now using ${JavaVersion.current().majorVersion} java version for building.")
 
 rootProject.name = "ui-debugger"
 include("mod-src")
